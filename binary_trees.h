f7e8ff97c96e267f8cfe2f26b0fe37d4dd1190ec
ezzkessem8 <92451799+ezzkessem8@users.noreply.github.com>
2024-03-30 18:04:37 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,12 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <limits.h>
 
 /* Basic Binary Tree Structure */
 typedef struct binary_tree_s binary_tree_t;
@@ -23,6 +29,13 @@ struct binary_tree_s
     struct binary_tree_s *left; /* Pointer to the left child node */
     struct binary_tree_s *right; /* Pointer to the right child node */
 };
+
+/* Queue Structure */
+typedef struct queue_s {
+    struct binary_tree_s *node;
+    struct queue_s *next;
+} queue_t;
+
 
 /* Function Prototypes */
 
@@ -97,5 +110,23 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 /* Task 23 */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
+queue_t *push_to_queue(queue_t **rear, binary_tree_t *node);
+binary_tree_t *pop_from_queue(queue_t **front);
 
 #endif /* BINARY_TREES_H */
