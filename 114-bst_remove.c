be4dce563c55e95a08a908c5219e17bff681ff82
ezzkessem8 <92451799+ezzkessem8@users.noreply.github.com>
2024-03-30 19:17:31 +0300

    Update 114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -16,19 +16,20 @@ bst_t *bst_remove(bst_t *root, int value)
     else if (value > root->n)
         root->right = bst_remove(root->right, value);
     else {
-        bst_t *temp;
         if (root->left == NULL) {
-            temp = root->right;
+            bst_t *temp = root->right;
             free(root);
             return temp;
         } else if (root->right == NULL) {
-            temp = root->left;
+            bst_t *temp = root->left;
             free(root);
             return temp;
         }
-        temp = root->right;
+
+        bst_t *temp = root->right;
         while (temp->left != NULL)
             temp = temp->left;
+
         root->n = temp->n;
         root->right = bst_remove(root->right, temp->n);
     }
